a6e706637ae8920e53623c5b0141bac6de1909b6
StrangeMD <StrangeMD@users.noreply.github.com>
2019-10-30 12:02:40 -0500

    Update DatabaseInterface.java

--- src/Database/DatabaseInterface.java
+++ src/Database/DatabaseInterface.java
@@ -5,5 +5,38 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public interface DatabaseInterface {
-
+  private connection = null;
+  
+  public boolean putUser(User u) {
+    //todo
+    try {
+      connection = this.getConnection();
+      Statement s = connection.getStatement();
+      if (u.getUserAccountNumber != null) {
+        s.executeUpdate("insert into users values (u.getUserName, u.getAccountNumber");
+  }
+  
+  public boolean putAccount(Account a) {
+    //todo
+  }
+  
+  public boolean getUser(String uid) {
+    //todo
+  }
+  
+  public boolean getAccount(String aid) {
+    //todo
+  }
+  
+  private Connection getConnection() {
+    try {
+      connection = DriverManager.getConnection ("jdbc:mysql://localhost/BankManagementSystem", "root", "password");
+    }
+    catch(ClassNotFOundException nfe) {
+      connection = null;
+    }
+    catch(SQLException sqle) {
+      connection = null;
+    }
+    return connection;
 }
